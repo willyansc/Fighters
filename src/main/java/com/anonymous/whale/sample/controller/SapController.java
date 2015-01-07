663f7bf43f3fc44379273ac3f9bac29abc0d9d3c
秦浏杰 <qinliujie@meituan.com>
2015-01-07 12:08:07 +0800

    restful

--- src/main/java/com/anonymous/whale/sample/controller/SapController.java
+++ src/main/java/com/anonymous/whale/sample/controller/SapController.java
@@ -8,6 +8,7 @@ import com.anonymous.whale.sample.service.ISampleService;
 import com.sun.tracing.dtrace.Attributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,10 +23,11 @@ public class SapController {
     private ISampleService sampleServiceImpl;
 
     @ResponseBody
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public APIResult getAllProvinces() {
-        Sample sample = sampleServiceImpl.getById(1);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public APIResult getAllProvinces(@PathVariable int id) {
+        Sample sample = sampleServiceImpl.getById(id);
         return APIUtils.getResponse(sample);
     }
+
 }
 
